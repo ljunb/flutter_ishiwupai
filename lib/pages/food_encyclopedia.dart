06d7624e46be0177016467591bcce7e5b317ef4a
ljunb <cookiejlim@gmail.com>
2018-12-10 22:16:51 +0800

    [update] 首页头图完成

--- lib/pages/food_encyclopedia.dart
+++ lib/pages/food_encyclopedia.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/home_search_bar.dart';
 
 class FoodEncyclopedia extends StatefulWidget {
   static String tag = 'food_encyclopedia_page';
@@ -12,12 +13,61 @@ class _FoodEncyclopediaState extends State<FoodEncyclopedia>
   @override
   bool get wantKeepAlive => true;
 
+  _handleClickSearchBar() {
+    print('handle');
+  }
+
+  _handleClickScanBtn() {
+    print('scan');
+  }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
 
-    return Center(
-      child: Text('食物百科'),
+    final screen = MediaQuery.of(context).size;
+    final iphoneXNavHeight = MediaQuery.of(context).padding.top;
+    final imgHeight = 240 + iphoneXNavHeight;
+
+    final topImage = Stack(
+      children: <Widget>[
+        Image.asset('assets/img_home_bg.png',
+            height: imgHeight, width: screen.width, fit: BoxFit.cover),
+        Container(
+          width: screen.width,
+          height: imgHeight,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(height: iphoneXNavHeight + 5),
+              Image.asset('assets/ic_head_logo.png', height: 22, width: 60),
+              Container(height: 80),
+              Text(
+                '查 询 食 物 信 息',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w200),
+              ),
+              Container(height: 32),
+              HomeSearchBarButton(
+                onPress: _handleClickSearchBar,
+                onScan: _handleClickScanBtn,
+              )
+            ],
+          ),
+        )
+      ],
+    );
+
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[topImage],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/home_search_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomeSearchBarButton extends StatelessWidget {
+  HomeSearchBarButton({Key key, @required this.onPress, @required this.onScan});
+
+  final Function onPress;
+  final Function onScan;
+
+  @override
+  Widget build(BuildContext context) {
+    final searchContent = Row(
+      children: <Widget>[
+        Image.asset('assets/ic_home_search.png', height: 20, width: 20),
+        Container(width: 8),
+        Text('请输入食物名称', style: TextStyle(color: Colors.orange, fontSize: 15)),
+      ],
+    );
+
+    final scanBtn = GestureDetector(
+      child: Image.asset('assets/ic_scan.png',
+          height: 34, width: 34, color: Colors.black),
+      onTap: this.onScan,
+    );
+
+    final content = Padding(
+        padding: EdgeInsets.fromLTRB(16, 0, 5, 0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[searchContent, scanBtn],
+        ));
+
+    return GestureDetector(
+        onTap: this.onPress,
+        child: Container(
+            height: 44,
+            width: MediaQuery.of(context).size.width * 0.88,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(4.0))),
+            child: content));
+  }
+}
